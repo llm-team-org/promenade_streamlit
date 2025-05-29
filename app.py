15f6da65458eba6f5b1d3437349b14eb2a6f42e6
viboognesh-doaz <viboognesh@doaz.ai>
2025-05-29 09:10:21 +0530

    Add another line

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from load_files import process_files_and_get_chat_object
 from dotenv import load_dotenv
 load_dotenv()
 import logging
-logging.info(os.getenv("OPENAI_API_KEY"))
+logging.info("OPENAI_KEY"+str(os.getenv("OPENAI_API_KEY")))
 from sec_agent import run_agent
 # Apply nest_asyncio to handle asyncio in Streamlit
 nest_asyncio.apply()
