5742d60997a90c8d9dbf33cc9220efb109058615
Tayyab-ai-07 <tayyab.mazhar@doaz.ai>
2025-06-11 11:34:26 +0500

    Dart references

--- prom_functions.py
+++ prom_functions.py
@@ -561,7 +561,7 @@ async def dart_get_report(query: str, report_source:str, path: str) -> tuple[str
     query= f"""
             Use this tone for report generation : Simple/Factual tone
             {query} 
-            -Must Use Dart fss filing references.
+            -In References, Must include Dart fss filing of company as reference.
             
             For the first page of report add Table with this data {table_data} put the value and information of these after you generate the report and have their value.
             Table format should be like this: {table_format}
